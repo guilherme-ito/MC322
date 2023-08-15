ca4253b03805327c14c14f7838d8807889074bc6
guilherme-ito <g238706@dac.unicamp.br>
2023-08-14 22:12:07 -0300

    teste2

--- src/livro/Livro.java
+++ src/livro/Livro.java
@@ -3,7 +3,7 @@ import livro.Emprestimo;
 
 public class Livro {
 
-    //Atributos da classe opa
+    //Atributos da classe 
     private String autor;
     private String titulo;
     private int edicao;
@@ -16,5 +16,21 @@ public class Livro {
         this.titulo = titulo;
         this.edicao = edicao;
         this.ano = ano;
+    }
+    //Getters e Setters da classe
+    public String getAutor () {
+    	return autor;
+    }
+    
+    public void setAutor(String autor) {
+    	this.autor = autor;
+    }
+    
+    public String getTitulo() {
+    	return titulo;
+    }
+    
+    public void seTitulo(String titulo) {
+    	this.titulo = titulo;
     }
 }
