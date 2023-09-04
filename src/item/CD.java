878bdfb9af2eae37f3172039c1d1986225baba03
n-thiago <nadimthiago@gmail.com>
2023-09-03 23:51:20 -0300

    arrumei os atributos dos itens

--- src/item/CD.java
+++ src/item/CD.java
@@ -3,13 +3,23 @@ package item;
 public class CD extends Item {
 
 //	fields
-	int faixas;
+	private String faixas;//lista
+	private String duracao;
+	private int exemplares;
+	private String conservacao;//enum?
+	//disponiveis
 	
 	
 //	constructor
-	public CD(String titulo,String idioma,String genero,int lancamento,boolean disponivel,String sinopse,String capa,String classificacao,int faixas) {
+	public CD(String titulo,String idioma,String genero,int lancamento,boolean disponivel,String sinopse,String capa,String classificacao,
+			String faixas, String duracao,int exemplares, String conservacao) {
+		
 		super(titulo,idioma,genero,lancamento,disponivel,sinopse,capa,classificacao);
 		this.faixas=faixas;
+		this.duracao=duracao;
+		this.exemplares=exemplares;
+		this.conservacao=conservacao;
+		
 	}
 	
 //	methods
@@ -17,13 +27,34 @@ public class CD extends Item {
 	
 
 //  getters
-  public int getFaixas() {
+  public String getFaixas() {
       return faixas;
+  }
+  public String getDuracao() {
+      return duracao;
+  }
+
+  public int getExemplares() {
+      return exemplares;
+  }
+
+  public String getConservacao() {
+      return conservacao;
   }
 
 //setters
-  public void setFaixas(int faixas) {
+  public void setFaixas(String faixas) {
       this.faixas = faixas;
   }
-  
+  public void setDuracao(String duracao) {
+      this.duracao = duracao;
+  }
+
+  public void setExemplares(int exemplares) {
+      this.exemplares = exemplares;
+  }
+
+  public void setConservacao(String conservacao) {
+      this.conservacao = conservacao;
+  }
 }
