40c1c2788f77b6bd85a0558ae92f44a09dfa26ae
n-thiago <nadimthiago@gmail.com>
2023-09-09 10:58:02 -0300

    continuei as classes inners

--- src/diversos/Multimidia.java
+++ src/diversos/Multimidia.java
@@ -1,9 +1,19 @@
 package diversos;
 
+import item.CD;
+import item.DVD;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Map ;
+
+
 public class Multimidia {
 
 	public class Audio{
-		
+		private CD detalhes;
+		private Set <String> podcasts;
+		private Map <String,String> gravacaoPalestra;
 	}
 	public class Video{
 		
--- src/diversos/ReservaSala.java
+++ src/diversos/ReservaSala.java
@@ -1,29 +1,45 @@
 package diversos;
 
+import java.util.HashMap;
+import java.util.Map ;
+
 public class ReservaSala {
 //	fields
 	
 	private String horaInicio;
-	private String horaFim;
+	private String horaFim;         //getters e setters?
 	private String dataReserva;
 	
 //	inner classes
 	
 	public class SalaIndividual{
-		
+		//fields
+		private boolean dispComp;
+		private int salaNum;
 	}
 	public class SalaGrupo{
-		
+		//fields
+		private int capcMAx;
+		private boolean projetor;
 	}
 	public class SalaSilenciosa{
-		
+		//fields
+		private int assentosNum;
+		private boolean cabineIndiv;
 	}
-	public class SalaMUltimidia{
+	public class SalaMultimidia{
+		Map<String,Integer> equipDisp;
 		
+		 public SalaMultimidia() {
+		        equipDisp = new HashMap<>();
+		        equipDisp.put("computador", 5);
+		        equipDisp.put("teclado", 7);
+		        equipDisp.put("fone", 6); 
 	}
 	
 	enum StatusReserva{
 		PENDENTE,CONFIRMADA,CANCELADA
 	}
 	
+	}
 }
