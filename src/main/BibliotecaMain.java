383bc2984876c3c4c43143a62118592233b3d4d1
Guilherme Henrique Ichiro Seto Ito <ra238706@tolstoy.lab.ic.unicamp.br>
2023-09-14 20:46:47 -0300

    adicionei uns negocio na main que é o que deu tempo

--- src/main/BibliotecaMain.java
+++ src/main/BibliotecaMain.java
@@ -3,6 +3,11 @@ package main;
 public class BibliotecaMain {
 	
 	 public static void main(String[] args) {
+
+			List<Membro> membros = newLinkedList<>();
+			Set<Emprestimo> emprestimos = newTreeSet<>();
+			List<Reserva> reservas = newLinkedList <>();
+		
 	        System.out.println("Olá, mundo");
 	    }
 	
